74c63e1c2dac47752c94546f5220761b9d0f89a5
protocol <1042805778@qq.com>
2026-02-07 15:35:47 +0800

    Update R_function.r

--- 03.Hepatocyte Ploidy Identification/R_function.r
+++ 03.Hepatocyte Ploidy Identification/R_function.r
@@ -1,44 +1,44 @@
-addPloidy<-function(df,num){
-library(dplyr)
-if (hasName(df,"cover_percentage") == FALSE){
+addPloidy <- function(df, num1, num2){
+  # 加载依赖包
+  library(dplyr)
+  # 计算覆盖率，与原逻辑保持一致
+  if (!hasName(df, "cover_percentage")){
     df$cover_percentage <- df$bright_area / df$area
+  }
+  df <- df[df$bright != 0, ]
+  df <- df[df$cover_percentage > 0.5, ]
+  df_aggregated <- df %>%
+    group_by(bright) %>%
+    summarise(
+      dapi_counts = sum(cover_percentage > 0.5),
+      ploidy_cu = paste(
+        case_when(
+          area < num1 ~ 2,     
+          area > num1 & area < num2 ~ 4, 
+          area > num2 ~ 8       
+        ), 
+        collapse = "_"
+      ),
+      area_combined = paste(area, collapse = "_"),
+      ploidy = sum(
+        case_when(
+          area < num1 ~ 2,
+          area > num1 & area < num2 ~ 4,
+          area > num2 ~ 8
+        )
+      ),
+      dapi_area = sum(area),
+      .groups = "drop"  
+    )
+  df_filter <- df_aggregated[df_aggregated$dapi_counts %in% c(1, 2), ]
+  df_filter <- as.data.frame(df_filter)
+  if(nrow(df_filter) > 0){
+    rownames(df_filter) <- df_filter[, 1]
+  }
+  
+  return(df_filter)
 }
-    df<-df[df$bright != 0,]
-    df<-df[df$cover_percentage>0.5,]
-df_aggregated <- df %>%
-  group_by(bright) %>%
-  summarise(
-    dapi_counts = sum(cover_percentage > 0.5),
-    ploidy_cu = paste(ifelse(area < num, 2, 4), collapse = "_"),
-    area_combined = paste(area, collapse = "_"),
-    ploidy = sum(ifelse(area<num,2,4)) ,
-    dapi_area=sum(area)
-  ) %>%
-  ungroup()
-    df_filter<-df_aggregated[df_aggregated$dapi_counts %in% c(1,2),]
-    df_filter<-as.data.frame(df_filter)
-    rownames(df_filter)<-df_filter[,1]
 
-    return(df_filter)
-}
-
-new_ploidy<-function(df,x){
-df<-df %>%
-    rowwise() %>%
-    mutate(
-        areas = list(strsplit(as.character(area_combined),"_"))[[1]],
-        ploidy_cu_new = paste(sapply(areas, function(area) ifelse(as.numeric(area) > x, 4, 2)), collapse = "_")  ,
-        ploidy_new = sum(as.numeric(strsplit(ploidy_cu_new, "_")[[1]]))                                    
-    ) %>%
-    ungroup() %>%
-    select(-areas)
-df <- df %>%
-    mutate(
-        ploidy_cu = ploidy_cu_new,
-        ploidy=ploidy_new
-    ) %>%
-return(df)    
-}
 
 gem2rds<-function(gem_path){
 gem=fread(gem_path)
